4fd018d4e69d83201395b344fc3c55c15d1fc053
Tomo-53 <zhiyedazhu49@gmail.com>
2025-10-13 18:59:59 +0900

    feat: グループ権限チェックシステムを実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\GroupController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,6 +16,37 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // ===== グループ関連のルート =====
+
+    // 認証のみ必要なグループ機能
+    Route::resource('groups', GroupController::class)->only(['index', 'create', 'store']);
+
+    // グループ参加申請（認証のみ必要）
+    Route::post('/groups/{group}/join', [GroupController::class, 'join'])->name('groups.join');
+
+    // ===== 特定グループでの権限が必要なルート =====
+    Route::prefix('groups/{group}')->group(function () {
+
+        // レベル2以上：一般メンバー権限
+        Route::middleware('check.group.permission:2')->group(function () {
+            Route::get('/', [GroupController::class, 'show'])->name('groups.show');
+        });
+
+        // レベル3以上：管理者権限
+        Route::middleware('check.group.permission:3')->group(function () {
+            Route::get('/members', [GroupController::class, 'members'])->name('groups.members');
+            Route::post('/members/{user}/approve', [GroupController::class, 'approveMember'])->name('groups.approve-member');
+            Route::delete('/members/{user}', [GroupController::class, 'removeMember'])->name('groups.remove-member');
+        });
+
+        // レベル4のみ：オーナー権限
+        Route::middleware('check.group.permission:4')->group(function () {
+            Route::get('/edit', [GroupController::class, 'edit'])->name('groups.edit');
+            Route::put('/', [GroupController::class, 'update'])->name('groups.update');
+            Route::delete('/', [GroupController::class, 'destroy'])->name('groups.destroy');
+        });
+    });
 });
 
 require __DIR__.'/auth.php';
