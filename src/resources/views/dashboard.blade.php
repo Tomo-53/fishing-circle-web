f7e8d7cc25868242c5d9113f9bca10ee09fec8ee
Tomo-53 <zhiyedazhu49@gmail.com>
2025-10-28 20:00:01 +0900

    style : 英語の部分日本語に変更

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -84,7 +84,7 @@
                                                 'bg-yellow-100 text-yellow-800' => $group->pivot->permission_level == 1,
                                                 'bg-blue-100 text-blue-800' => $group->pivot->permission_level == 2,
                                                 'bg-red-100 text-red-800' => $group->pivot->permission_level == 3,
-                                                'bg-purple-100 text-purple-800' => $group->pivot->permission_level == 4,
+                                                'bg-green-100 text-green-800' => $group->pivot->permission_level == 4,
                                             ])>
                                                 @switch($group->pivot->permission_level)
                                                     @case(4) オーナー @break
