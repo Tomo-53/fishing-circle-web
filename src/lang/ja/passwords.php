c27c74c92941b56b84318e1b45b463dbd14f5780
Tomo-53 <zhiyedazhu49@gmail.com>
2025-11-09 14:11:44 +0900

    エラーメッセージ日本語対応

---
+++ src/lang/ja/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | outcome such as failure due to an invalid password / reset token.
+    |
+    */
+
+    'reset' => 'パスワードがリセットされました。',
+    'sent' => 'パスワードリセットリンクをメールで送信しました。',
+    'throttled' => '少し時間をおいてから再度お試しください。',
+    'token' => 'パスワードリセットトークンが無効です。',
+    'user' => '指定されたメールアドレスのユーザーが見つかりません。',
+
+];
