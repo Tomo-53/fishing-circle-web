d74269c918e5baa56194670141cc5aefbb48c74f
Tomo-53 <zhiyedazhu49@gmail.com>
2025-11-10 15:27:14 +0900

    fix : パスワード再設定

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Notifications\ResetPasswordNotification;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -106,5 +107,13 @@ class User extends Authenticatable
             ->first();
 
         return $userGroup ? $userGroup->is_approved : false;
+    }
+
+    /**
+     * パスワードリセット通知を送信
+     */
+    public function sendPasswordResetNotification($token): void
+    {
+        $this->notify(new ResetPasswordNotification($token));
     }
 }
